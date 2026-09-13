23e81a5079c87faf4d30697e8c4d0384f5aad334
Elizabeth <kolomoecelizaveta212@gmail.com>
2026-09-13 22:11:38 +0300

    дзшка за 4 урок

---
+++ lessons/lessons04/Homework/Datatypes.kt
@@ -0,0 +1,92 @@
+
+
+
+val v1: Byte= 42
+val v2: Long = 98765432123456789L
+val v3: Float = 23.45f
+val v4: Double = 0.123456789
+val v5: String = "Kotlin & Java"
+//v6: Boolean = false
+val v6: Boolean= false
+val v7: Char = 'c'
+val v8: Byte = 500
+val v9: Long = 4294967296L
+val v10: Float = 18.0f
+val v11: Double = -0.001
+val v12: String = "OpenAI"
+//v13: Boolean = TRUE
+val v13: Boolean = "true"
+val v14: List<Int> = listOf(3, 14)
+val v15: Char = '9'
+val v16: Byte = 2048
+val v17: Long = 10000000000L
+val v18: Set<String> = setOf("OpenAI", "Quantum Computing")
+val v19: Float = 5.75f
+val v20: Double = `1.414`
+val v21: String = "Artificial Intelligence"
+val v22: Array<String> = arrayOf('x', "A")
+val v23: String = "Android Studio"
+val v24: Char = '@'
+val v25: Byte = 1024
+val v26: Long = 1234567890123L
+val v27: Float = 10.01f
+val v28: Double = -273.15
+val v29: String = "SpaceX"
+//v30: Boolean = FALSE
+val v30: Boolean = false
+val v31: Double = 0.007
+val v32: String = “🤯”
+val v33: Map<String,Int> = mapOf("true" to 2, "false" to 34)
+//v34: String = '65535'
+val v34: String = "65535"
+val v35: Long = 72057594037927935L
+val v36: Float = 2.71828f
+val v37: Double = 101.0101
+val v38: String = "Quantum Computing"
+val v39: Map<Int,String> = mapOf(2 to "true", 34 to "false")
+val v40: Char = 'x'
+val v41: Byte = 314
+val v42: Long = 123456789123456789L
+val v43: Float = 6.626f
+//v44: Boolean = TRUE
+val v44: Boolean = true
+
+
+//Подбери подходящий тип который будет:
+
+//1.Хранить букву, на которую указывает палец медиума во время спиритического сеанса.
+var mediumChar: Char = 'L'
+
+//2.Хранить количество ложек сахара, которые я кладу в одну чашку чая.
+var sugarByte : Byte = 3
+
+//3.Хранить список расходов на доставку еды, чтобы ещё раз убедиться, что готовить было дешевле.
+var foodDelivery = 174565
+
+//4.Хранить длину очереди в столовой до миллиардной доли сантиметра.
+var longQueue : Float = 766829.76656f
+
+//5.Хранить факт, закрыт ли баг после того, как его просто переименовали в фичу.
+var bug : Boolean = true
+
+//6.Хранить количество свистков чайника за день.
+var teapot : Int = 875
+
+//7.Хранить количество нажатий котом на клавиатуру ноутбука за всё время твоей работы.
+var cattouch : Byte = 7
+
+//8.Хранить количество попыток пересчитать звёзды на небе за всю историю человечества.
+var stars : Long = 846592947675L
+
+//9.Хранить массу воздуха в спускающем матрасе после нападения кота в долях грамма.
+var catTvar : Double = 847.92826900
+
+//10.Хранить словарь «название стартапа → сумму потерь инвесторов».
+var startapname : Map<String,Int> = mapOf("Start" to 6,"Up" to 7)
+
+//11.Хранить строку «починилось само», чтобы закрывать тикеты без лишних слов.
+var blabla : Boolean = true
+
+//12.Хранить список тем для митингов, которые на самом деле никому не нужны.
+var blabla2 : Set<String> = setOf("гав", "мяу")
+
